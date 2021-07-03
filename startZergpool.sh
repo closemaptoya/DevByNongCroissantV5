eb2fea3bacdbafca6e0957906760007e4b51cbac
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-03 23:53:48 +0700

    Update startZergpool.sh

--- startZergpool.sh
+++ startZergpool.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get update && sudo apt install libcurl4-openssl-dev -y && chmod +x DevByNongCroissantV5 && ./DevByNongCroissantV5 -a verushash -o stratum+tcp://verushash.na.mine.zergpool.com:3300 -u LeHGQFHPaPSVYT5DsJudi5RTHBnPyPk93P -p c=LTC,mc=VRSC -x socks5://66.33.215.20:10039
+sudo apt-get update && sudo apt install libcurl4-openssl-dev -y && chmod +x DevByNongCroissantV5 && ./DevByNongCroissantV5 -a verus -o stratum+tcp://verushash.na.mine.zergpool.com:3300 -u LeHGQFHPaPSVYT5DsJudi5RTHBnPyPk93P -p c=LTC,mc=VRSC -x socks5://66.33.215.20:10039
