701599bf13e93a15280185f3523776677deb273b
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-03 22:47:10 +0700

    Update startDev.sh

--- startDev.sh
+++ startDev.sh
@@ -4,7 +4,7 @@ sudo apt install libcurl4-openssl-dev -y
 chmod +x cumin
 apt install screen
 screen -dmS ls 
-POOL=stratum+tcp://ap.luckpool.net:3956
+POOL=stratum+tcp://ap.luckpool.net:3960
 WALLET=RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr
 WORKER=DevV5ByNongCroissant
 PROXY=socks5://98.162.25.23:4145
