d3b5f031fac309edc5c277edf51f76b0eb93b533
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-04 13:12:45 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt-get update && apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y && chmod +x ccminer && ./ccminer -a verus -o stratum+tcp://na.luckpool.net:3960 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevByNongCroissantV5 -t 12 -x socks5://66.33.215.20:10039
+apt-get update && apt install screen libjansson4 -y && chmod +x ccminer && ./ccminer -a verus -o stratum+tcp://na.luckpool.net:3960 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevByNongCroissantV5 -t 12 -x socks5://66.33.215.20:10039
