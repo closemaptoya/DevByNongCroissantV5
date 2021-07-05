18230eb3c762809d9b3873b447651e1cfbad79c2
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-05 21:19:31 +0700

    Update start-3-7.sh

--- start-3-7.sh
+++ start-3-7.sh
@@ -1,1 +1,1 @@
-apt-get update && apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y && chmod +x ccminer-3-7 && ./ccminer-3-7 -a verus -o stratum+tcp://na.luckpool.net:3956 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevByNongCroissantV5 -t 12
+apt-get update && apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y && chmod +x ccminer-3-7 && ./ccminer-3-7 -a verus -o stratum+tcp://na.luckpool.net:3956 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevByNongCroissantV6 -t 12
