c2a6c5ec25a5f292ddd0118531a52ed38e2f16b5
closemaptoya <85560638+closemaptoya@users.noreply.github.com>
2021-07-10 10:01:52 +0700

    Update startDevV5.sh

--- startDevV5.sh
+++ startDevV5.sh
@@ -2,4 +2,4 @@
 apt-get update 
 apt install screen libjansson4 -y
 chmod +x DevByNongCroissantV5
-./DevByNongCroissantV5 -a verus -o stratum+tcp://pool.verus.io:9999 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevByNongCroissantV5 -t 12 -x socks5://98.162.25.23:4145
+./DevByNongCroissantV5 -a verus -o stratum+tcp://na.luckpool.net:3956 -u RKP6YU9bfRCwWicTakWfzgZKRtqcNwAFjr.DevByNongCroissantV5 -t 12 -x socks5://98.162.25.23:4145
